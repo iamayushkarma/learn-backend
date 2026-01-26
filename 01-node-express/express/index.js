bdb3849fe5e85b8524b21e01fa90c818a270a6b5
Ayush Karma <ayushkarma.dev@gmail.com>
2026-01-26 11:53:37 +0530

    Add handeling 404 route

--- 01-node-express/express/index.js
+++ 01-node-express/express/index.js
@@ -16,6 +16,11 @@ app.get("/", (req, res) => {
     `);
 });
 app.use(homeRouter); // add home route
+
+// for handeling 404 page, must be at end
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404 Page not found</h1>");
+});
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:3002/`);
 });
