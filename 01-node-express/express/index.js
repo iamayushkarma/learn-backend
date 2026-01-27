4b45c2919d0a8754c64ccb3fa2f3049af5129ad9
Ayush Karma <ayushkarma.dev@gmail.com>
2026-01-27 14:49:20 +0530

    Add server listening only on db connection

--- 01-node-express/express/index.js
+++ 01-node-express/express/index.js
@@ -25,7 +25,14 @@ app.use("/api/users", userRouter);
 app.use((req, res, next) => {
   res.status(404).send("<h1>404 Page not found</h1>");
 });
-connectDB();
-app.listen(PORT, () => {
-  console.log(`server listening on http://localhost:3002/`);
-});
+
+(async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`server listening on http://localhost:3002/`);
+    });
+  } catch (err) {
+    console.error("Failed to start server", err);
+  }
+})();
