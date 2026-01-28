1724809e0faca3d60e879fd9f042b31f198f5497
Ayush Karma <ayushkarma.dev@gmail.com>
2026-01-28 09:21:00 +0530

    Set up initial EJS in url shortener project for SSR

--- 01-node-express/projects/url-shortener/index.js
+++ 01-node-express/projects/url-shortener/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const router = require("./router/url.router");
 const { connectToMongoDb } = require("./connection");
 const URL = require("./model/url.model");
@@ -7,8 +8,16 @@ const URL = require("./model/url.model");
 const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.get("/", (req, res) => {
   res.send("its Live");
+});
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
 });
 
 app.use("/url", router);
@@ -32,7 +41,7 @@ app.get("/:shortId", async (req, res) => {
   try {
     await connectToMongoDb(MONGO_URI);
     const PORT = process.env.PORT || 3000;
-    app.listen(process.env.PORT, (req, res) => {
+    app.listen(PORT, (req, res) => {
       console.log(`server listening on http://localhost:${process.env.PORT}/`);
     });
   } catch (error) {
